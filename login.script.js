453c7329805431193f1f50fbba6789d34332529f
Diogo Asenjo <diogoasenjo@outlook.com>
2023-01-30 16:57:39 -0300

    Inicializando o JS

---
+++ login.script.js
@@ -0,0 +1,6 @@
+const usuario = document.getElementById("usuario").value;
+const senha = document.getElementById("senha").value;
+
+if (usuario === "123" && senha === "123") {
+    alert ("Login realizado com sucesso")
+}
